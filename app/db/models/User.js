7080adec30cf0c8352830c741472ed84ad784fb2
risenforces <risenforces@gmail.com>
2019-05-12 20:01:21 +0500

    add timestamps and some methods to UserSchema

--- app/db/models/User.js
+++ app/db/models/User.js
@@ -1,34 +1,38 @@
 const { Schema, model } = require('mongoose')
 const bcrypt = require('bcrypt')
+const omit = require('deep-omit')
 
-const UserSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    index: {
-      unique: true
-    }
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  role: {
-    type: String,
-    enum: ['user', 'admin'],
-    default: 'user'
-  },
-  status: {
-    banned: {
-      type: Boolean,
-      default: false
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      index: {
+        unique: true
+      }
     },
-    mutedUntil: {
-      type: Date,
-      default: null
+    password: {
+      type: String,
+      required: true
+    },
+    role: {
+      type: String,
+      enum: ['user', 'admin'],
+      default: 'user'
+    },
+    status: {
+      banned: {
+        type: Boolean,
+        default: false
+      },
+      mutedUntil: {
+        type: Date,
+        default: null
+      }
     }
-  }
-})
+  },
+  { timestamps: true }
+)
 
 UserSchema.pre('save', function(next) {
   const user = this
@@ -45,6 +49,14 @@ UserSchema.pre('save', function(next) {
 
 UserSchema.methods.comparePassword = function(password) {
   return bcrypt.compareSync(password, this.password)
+}
+
+UserSchema.methods.getFullData = function() {
+  return omit(this, 'password')
+}
+
+UserSchema.methods.getPublicData = function() {
+  return omit(this, ['password', 'status.mutedUntil', 'updatedAt'])
 }
 
 const User = model('user', UserSchema)
