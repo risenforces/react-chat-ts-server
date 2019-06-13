94fca27e30bb345ca70e85ede55a79231fac9ec1
risenforces <risenforces@gmail.com>
2019-06-13 20:01:52 +0500

    add alias

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,7 @@ module.exports = {
     '^@app(.*)$': '<rootDir>/app$1',
     '^@rest-v1(.*)$': '<rootDir>/app/rest/v1',
     '^@socket(.*)$': '<rootDir>/app/socket',
+    '^@common-schemas$': '<rootDir>/app/constants/common-schemas',
     '^@config$': '<rootDir>/app.config.js'
   }
 }
